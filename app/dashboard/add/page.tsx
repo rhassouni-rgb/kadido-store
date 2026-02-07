571a6161c490b04fb6ca0ecfe29b7f3a032ebd5b
Raed <raed@gmail.com>
2026-02-08 00:44:05 +0100

    Fix shop id error

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -42,7 +42,7 @@ export default function AddProductPage() {
           { 
             name, 
             price, 
-            shop_id: shop.id 
+           shop_id: shop!.id
           }
         ])
 
