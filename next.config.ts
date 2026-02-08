d4e29db652f2c2282a646cbae1a2059487febcb7
Raed <raed@gmail.com>
2026-02-09 00:56:25 +0100

    Fix sanity env final

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io', // 👈 هذا يسمح بجلب الصور من Sanity
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // (اختياري) للصور المؤقتة إذا كنت تستخدمها
+      }
+    ],
+  },
 };
 
 export default nextConfig;
