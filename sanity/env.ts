2c8ed664aa1a74da71a3d3b0f7e98781dc5728ea
Raed <raed@gmail.com>
2026-02-09 00:26:40 +0100

    Fix env file hardcoded

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,7 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-29'
+export const apiVersion = '2024-01-01'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = 'production'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = 'q3ui6u4i'
 
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
+export const useCdn = false
