1bbfa626ed5657b42df2696d0b715210a1b500c2
Raed <raed@gmail.com>
2026-02-08 01:33:33 +0100

    Force vercel update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+لاىةلاىةimport React, { useState, useEffect, useRef } from "react";
 import { ShoppingCart, Search, ArrowRight, Phone, MessageCircle, Instagram, Facebook, Music, Grid } from "lucide-react"; 
 import { client } from "@/sanity/lib/client"; 
 import imageUrlBuilder from "@sanity/image-url";
